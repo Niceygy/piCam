11b7af05533477747bfd1cc9ff24ff0ca85678b9
NiceygyLive <niceygy@niceygylive.xyz>
2023-07-18 11:26:51 +0000

    added start

--- app/main.py
+++ app/main.py
@@ -12,65 +12,17 @@ def startup(): # check if it need to create photo and video dirs
     else:
         return
 
-
-def f1():
-    cameraControl.liveCam()
-
-def f2():
-    IO.checkForLightsMove()
-
-def f3():
-    IO.checkForLightsToggle()
+    
 
 def main():
     #startup()
     LEDcontrol.LEDon()
-    # if (argOne != None): 
-    #                 print("Using automatic input")
-    #                 operation = int(argOne)
-    # else:
-         
-    #     print("Select operation: ")
-    #     print("1. Live feed")
-    #     print("2. Take image")
-    #     print("3. Take a video of x seconds")
-    #     print("4. Take an image every x seconds")
-    #     print("5. Exit")
-    
-    #     operation = int(input())
-
-    # print(operation)
-    # if (operation == 1):
-    #     cameraControl.liveCam()
-    # elif (operation == 2):
-    #     cameraControl.takePhoto()
-    # elif (operation == 3):
-    #     videoTime = input("How many seconds? ")
-    #     cameraControl.video(int(videoTime))
-    # elif (operation == 4):
-    #     secs = input("How many seconds between photos? ")
-    #     num = input("How many photos? ")
-    #     cameraControl.photoEverySec(secs, num)
-    # elif (operation == 5):
-    #     return
-    # elif (operation == 6):
-    #     print("Debug mode starting...")
-    #     print("Live feed started for 5 seconds")
-    #     cameraControl.liveCam()
-    #     print("Taking photo...")
-    #     cameraControl.takePhoto()
-    #     print("Taking photo every 2 seconds for 10 seconds...")
-    #     cameraControl.photoEverySec(2, 5)
-    #     print("Debug mode finished!")
-    # else:
-    #     print("Invalid input")
-    #     main()
 
     operation = int(argOne)
     if (operation == 1):
         print("Starting live feed")
-        f3()
-        f2()
-        f1()
+        IO.checkForLightsToggle()
+    elif (operation == 2):
+        IO.checkForLightsMove()
     
 main()
