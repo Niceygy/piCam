17ae8aeb50931faaf18e0a0d93f6127cd6ed0718
NiceygyLive <niceygy@niceygylive.xyz>
2023-07-18 12:04:41 +0000

    moved buttons into one fucntion

--- app/gpio.py
+++ app/gpio.py
@@ -1,20 +1,30 @@
-#on button press, move light 3 ones to the left (clockwise)
-#GPIO List: 
+# on button press, move light 3 ones to the left (clockwise)
+# GPIO List:
 #   Lights: Pin 2 (5v), Pin 18 (Data), Pin 14 (Ground)
 #   Buttons: Pin 1 (3v3), Pin {17, 22, 27} (Inputs), Pin 20 (Ground)
 from gpiozero import Button
 from signal import pause
 import ledControl as LEDcontrol
 
-def checkForLightsToggle():
-    LTbutton = Button(17)
-    LTbutton.when_pressed = LEDcontrol.LEDtoggle
-    pause()
+# def checkForLightsToggle():
+#     LTbutton = Button(17)
+#     LTbutton.when_pressed = LEDcontrol.LEDtoggle
+#     pause()
 
 
-def checkForLightsMove():
-    LMbutton = Button(22)
-    print("CheckForLightsMove function running...")
+# def checkForLightsMove():
+#     LMbutton = Button(22)
+#     print("CheckForLightsMove function running...")
+#     LMbutton.when_pressed = LEDcontrol.rotateLED
+#     pause()
+
+
+def handleButtons():
+    LMbutton = Button(22)  # Light Move
+    LTbutton = Button(17)  # Light toggle
+    print("Button handler running")
+
     LMbutton.when_pressed = LEDcontrol.rotateLED
-    pause()
+    LTbutton.when_pressed = LEDcontrol.LEDtoggle
 
+    pause()
--- app/main.py
+++ app/main.py
@@ -2,28 +2,28 @@ import ledControl as LEDcontrol
 import cameraControl as cameraControl
 import sys
 import gpio as IO
+
 argOne = sys.argv[1]
 
-def startup(): # check if it need to create photo and video dirs
+
+def startup():  # check if it need to create photo and video dirs
     firstStart = cameraControl.run("cat config.txt")
-    if (firstStart != "Version v1.1"):
+    if firstStart != "Version v1.1":
         cameraControl.run("touch config.txt && echo Version v1.1 >> config.txt")
         cameraControl.run("mkdir /home/$USER/picam && mkdir /home/$USER/pivid")
     else:
         return
 
-    
 
 def main():
-    #startup()
+    # startup()
     LEDcontrol.LEDon()
 
     operation = int(argOne)
-    if (operation == 1):
-        print("Starting LED toggle manager...")
-        IO.checkForLightsToggle()
-    elif (operation == 2):
-        print("Starting LED movement manager...")
-        IO.checkForLightsMove()
-    
+    if operation == 1:
+        print("Starting LED  manager...")
+        IO.handleButtons()
+
+
+
 main()
