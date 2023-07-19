76112778b4f853bf0384e9cfc41c772f0de0e268
NiceygyLive <niceygy@niceygylive.xyz>
2023-07-19 10:42:49 +0000

    updated mv cmd for windows

--- openCV/image.py
+++ openCV/image.py
@@ -2,6 +2,7 @@ from time import sleep
 import cv2
 import functions as func
 camera = cv2.VideoCapture("tcp://192.168.1.158:33") # type: ignore #listen to the tcp video strem from the PI
+import subprocess
 
 def compare(img, template):
     # load the input image and template image from disk, then display
@@ -72,7 +73,7 @@ def showDiffrences(img, template):
 def takeImage(i):
     return_value, image = camera.read()
     cv2.imwrite(str(i)+'.png', image)
-    func.run("mv "+i+".png images/")
+    subprocess.run("mv "+i+".png images/")
     print("Saved image as " + str(i) + ".png")
     res = "image"+str(i)+".png"
     return res
